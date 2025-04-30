6dbcf10d53b69a69b20c3f30b9970dbfb122ad56
Entity-now <2966388213@qq.com>
2025-04-30 23:47:25 +0800

    update package.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import TiptapAntDesign from '@/components/vue3-tiptap/index.vue'
+import TiptapAntDesign from './components/vue3-tiptap/index.vue'
 import type {
 	TiptapProps,
 	TiptapEmits
-} from '@/components/vue3-tiptap/vue3-tiptap'
+} from './components/vue3-tiptap/vue3-tiptap'
 
 export {
 	TiptapProps,
